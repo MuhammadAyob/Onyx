4dca27fdaee564a0284cad3f47479a93e4d14e88
MuhammadAyob <u20442409@tuks.co.za>
2023-06-06 22:51:15 +0200

    update

---
+++ INF370 2023 Web API/INF370 2023 Web API/Models/IApplicants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INF370_2023_Web_API.Models
+{
+    public interface IApplicants
+    {
+
+    }
+}
